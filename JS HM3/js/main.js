6b1f77fd932fd710c3d94ada31650e02bb521a5c
Stanislav Garanin <sats,garanin98@gmail.com>
2020-01-27 19:47:16 +0300

    add new function

--- JS HM3/js/main.js
+++ JS HM3/js/main.js
@@ -1,11 +1,21 @@
-let a = [2, 4, 60, 8, 10];
-let c = max(a);
-function max(a) {
-    let b = a[0];
-    for(let i = 0; i < a.length; i++) {
-        if (b < a[i]) 
-            b = a[i];
+// let a = [2, 4, 60, 8, 10];
+// let c = max(a);
+// function max(a) {
+//     let b = a[0];
+//     for(let i = 0; i < a.length; i++) {
+//         if (b < a[i]) 
+//             b = a[i];
+//     }
+//     return b;
+// }
+// alert(c);
+
+function max(array) {
+    let d = array[0];
+    for(let i = 0; i < array.length; i++) {
+        if (d < array[i]) 
+            d = array[i];
     }
-    return b;
+    return d;
 }
-alert(c);
+console.log(max([2, 4, 60, 8, 10]))
