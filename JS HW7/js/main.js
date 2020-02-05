6feed0ca868c80b1c9d984c86a127a00afadc603
Stanislav Garanin <sats,garanin98@gmail.com>
2020-02-05 18:22:25 +0300

    hw 7

---
+++ JS HW7/js/main.js
@@ -0,0 +1,18 @@
+const person = {
+    name: 'Stanislav'
+}
+function info(phone, email) {
+    console.log(`Имя: ${this.name}, Tel.:${phone}, Email: ${email}`)
+}
+
+function bind(fn, context, ...rest) {
+    return fn.bind(context, ...rest)
+}
+ bind(info, person)('375296433353', 'sgaranin@inbox.ru')
+ bind(info, person,'375296433353')('sgaranin@inbox.ru')
+ bind(info, person, '375296433353', 'sgaranin@inbox.ru')()
+
+
+
+
+
