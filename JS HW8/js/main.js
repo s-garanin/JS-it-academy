67eb848bca6b348fa5f32ca51d18063ad5bb10ee
Stanislav Garanin <sats,garanin98@gmail.com>
2020-02-08 03:13:48 +0300

    bug fixed

--- JS HW8/js/main.js
+++ JS HW8/js/main.js
@@ -4,30 +4,31 @@
 //     }
 //     addValue(key, value)
 // }
-
-function HashStorageFunc() {
-    this.addValue = function (key, value) {
-        this[key] = value;
-    }
-    this.getValue = function (key) {
-        return this[key];
-    }
-    this.deleteValue = function(key) {
-    if (this.key) {
-        delete this[key];
-        return true;
-    }
-    return false;
-    }
-    this.getKeys = function() {
-        return Object.keys(this);
+class HashStorageFunc {
+    constructor() {
+        this.addValue = function (key, value) {
+            this[key] = value;
+        };
+        this.getValue = function (key) {
+            return this[key];
+        };
+        this.deleteValue = function (key) {
+            if (this.key) {
+                delete this[key];
+                return true;
+            }
+            return false;
+        };
+        this.getKeys = function () {
+            return Object.keys(this);
+        };
     }
 }
 let drinkStorage = new HashStorageFunc();
 
-drinkStorage.addValue('milk', 'non-alc');
-drinkStorage.addValue('orange juice', 'non-alc');
-drinkStorage.addValue('wine', 'alc');
+drinkStorage.addValue('soda', 'non-alc');
+drinkStorage.addValue('juice', 'non-alc');
+drinkStorage.addValue('beer', 'alc');
 drinkStorage.addValue('mineral water', 'non-alc');
-drinkStorage.addValue('coke', 'non-alc');
+drinkStorage.addValue('wine', 'alc');
 console.log( drinkStorage.getValue('wine') );
