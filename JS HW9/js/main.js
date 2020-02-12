30a4380041b49a625475b19c2429fdc81b5e570d
Stanislav Garanin <sats,garanin98@gmail.com>
2020-02-12 17:25:46 +0300

    Update main.js

--- JS HW9/js/main.js
+++ JS HW9/js/main.js
@@ -1,30 +1,35 @@
 function HashStorage() {
-    let obj = {}
+    this._obj = {};
     this.addValue = function (key, value) {
-        obj[key] = value;
+        this._obj[key] = value;
     };
     this.getValue = function (key) {
-        return obj[key];
+        return this._obj[key];
     };
     this.deleteValue = function (key) {
-        if (obj.key) {
-            return delete obj.key;
+        if (this._obj.key) {
+            return delete this._obj.key;
         }
         return false;
     };
     this.getKeys = function () {
-        return Object.keys(obj);
+        return Object.keys(this._obj);
     };
 }
 let drinkStorage = new HashStorage();
 
 
 
-function foo () {
+function foo() {
     HashStorage.call(this);
     let a = this.addValue;
-    this.addValue = function(key, value) {
+    this.addValue = function (key, value) {
         a.call(this, key, value);
+        this.getStas();
+    }
+    this.getStas = function() {
+        console.log('Stas');
+        
     }
 }
 let bar = new foo();
